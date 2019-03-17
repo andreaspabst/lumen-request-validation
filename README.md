00219ef0c5354a4e3c6f69187cdc918756f927e0
Andreas Pabst MacBook Air <kontakt@andreas-pabst.de>
2019-03-17 17:32:47 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# lumen-request-validation
-Adds the Laravel ability to seperate validations from the controller
+# Lumen Request Validation
+Adds the well known Laravel ability to separate validations from the controller.
+
+## Installation
+First use composer to install the package.
+ 
+`composer require andreaspabst/lumen-request-validation`
+
+Next register the packages service provider in your *bootstrap/app.php* 
+
+`$app->register(AndreasPabst\RequestValidation\RequestServiceProvider::class);`
+
+Then you are able to create requests with *artisan* comparable to laravel 
+
+## How to call
+
+`php artisan make:request RequestName`
+
+The new class can be found under app/Http/Requests folder.
+
+## Example usage
+
+Use your Request class in a controller via method injection
+
+```php
+<?php 
+use App\Http\Requests\ExampleRequest;
+
+class ExampleController extends Controller
+{
+    public function index(ExampleRequest $request) {
+	    // ...
+    }
+    //...
+}
+```
+
+## Other stuff...
+Have Fun Using!
+
+# Security
+If you discover any security related issues, please email management@andreaspabst.com instead of using the issue tracker.
+
+# Postcardware
+You're free to use this package, but if it makes it to your product we highly appreciate you sending us a postcard from your hometown, mentioning which of our package(s) you are using.
+
+Our address is provided on [Andreas Pabst.com](https://www.andreaspabst.com)
+
+We publish all received postcards on our website.
+
+# Credits
+Andreas Pabst
+
+# License
+The MIT License (MIT). Please see License File for more information.
---
+++ src/RequestAbstract.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace AndreasPabst\RequestValidation;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Validation\ValidatesWhenResolvedTrait;
+use Illuminate\Contracts\Validation\ValidatesWhenResolved;
+use Illuminate\Contracts\Validation\Factory as ValidationFactory;
+
+abstract class RequestAbstract extends Request implements ValidatesWhenResolved
+{
+    use ValidatesWhenResolvedTrait;
+
+    /**
+     * The container instance.
+     *
+     * @var \Illuminate\Contracts\Container\Container
+     */
+    protected $container;
+
+    /**
+     * The key to be used for the view error bag.
+     *
+     * @var string
+     */
+    protected $errorBag = 'default';
+
+    /**
+     * Get the validator instance for the request.
+     *
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function getValidatorInstance()
+    {
+        $factory = $this->container->make(ValidationFactory::class);
+
+        if (method_exists($this, 'validator')) {
+            $validator = $this->container->call([$this, 'validator'], compact('factory'));
+        } else {
+            $validator = $this->createDefaultValidator($factory);
+        }
+
+        if (method_exists($this, 'withValidator')) {
+            $this->withValidator($validator);
+        }
+
+        return $validator;
+    }
+
+    /**
+     * Create the default validator instance.
+     *
+     * @param  \Illuminate\Contracts\Validation\Factory  $factory
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function createDefaultValidator(ValidationFactory $factory)
+    {
+        return $factory->make(
+            $this->validationData(), $this->container->call([$this, 'rules']),
+            $this->messages(), $this->attributes()
+        );
+    }
+
+    /**
+     * Get data to be validated from the request.
+     *
+     * @return array
+     */
+    protected function validationData()
+    {
+        return $this->all();
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new ValidationException($validator, $this->formatErrors($validator));
+    }
+
+    /**
+     * Format the errors from the given Validator instance.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function formatErrors(Validator $validator)
+    {
+        return new JsonResponse($validator->getMessageBag()->toArray(), 422);
+    }
+
+    /**
+     * Get custom messages for validator errors.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [];
+    }
+
+    /**
+     * Set the container implementation.
+     *
+     * @param  \Illuminate\Contracts\Container\Container  $container
+     * @return $this
+     */
+    public function setContainer(Container $container)
+    {
+        $this->container = $container;
+
+        return $this;
+    }
+}
